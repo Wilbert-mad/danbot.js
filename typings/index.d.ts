ebe61ec2af58e89c0cee9eba4ef3499c797d4575
Wilbert-mad <63362762+Wilbert-mad@users.noreply.github.com>
2020-09-19 21:25:48 -0500

    types updated

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -16,27 +16,33 @@ declare module 'danbot.js' {
   export class HostClient extends Base {
     public constructor(client: Client, key: string)
 
-    public autoPost(Time: number): Promise<void>
+    public posting: boolean;
+    public autoPost(Time?: number): Promise<void>
 
     on(event: string, listener: Function): this;
     on(event: 'post', listener: () => void): this;
     on(event: 'autoPosting', listener: () => void): this;
+    on(event: 'stop', listener: () => void): this;
     on(event: 'error', listener: (error: Error) => void): this;
+    on(event: 'REQUEST', listener: (response: Response) => void): this;
   }
 
   export class Host extends Base {
     public constructor(client: Client, key: string)
 
-    public autoPost(Time: number): Promise<void>
+    public posting: boolean;
+    public autoPost(Time?: number): Promise<void>
 
     on(event: string, listener: Function): this;
     on(event: 'post', listener: () => void): this;
     on(event: 'autoPosting', listener: () => void): this;
+    on(event: 'stop', listener: () => void): this;
     on(event: 'error', listener: (error: Error) => void): this;
+    on(event: 'REQUEST', listener: (response: Response) => void): this;
   }
 
   export class Utils {
-    public static request(method: methods, endpoint: string, data: Object): Promise<void>
+    public static request(method: methods, endpoint: string, data?: Object): Promise<void>
     public static shardingGetGuild(data: Base): number;
     public static shardingGetUsers(data: Base): number;
     public static getGuilds(data: Base): Collection<Snowflake, Guild>;
@@ -45,16 +51,42 @@ declare module 'danbot.js' {
     public static getUserID(data: Base): Snowflake;
   }
 
+  export interface Response {
+    raw: string,
+    body: { 
+      error: boolean, 
+      message: string
+    },
+    headers: {
+      date: string,
+      server: string,
+      etag: string,
+      connection: string,
+      'x-dns-prefetch-control': string,
+      'strict-transport-security': string,
+      'x-download-options': string,
+      'x-content-type-options': string,
+      'x-xss-protection': string,
+      'access-control-allow-origin': string,
+      'access-control-allow-methods': string,
+      'content-type': string,
+      'content-length': string,
+    }
+    status: number,
+    ok: boolean,
+    statusText: string
+  }
+
   export const Version: string;  
 
-  type methods = 
+  export type methods = 
   | 'get' 
   | 'post' 
   | 'put' 
   | 'patch' 
   | 'delete';
 
-  type ServerInfo = {
+  export interface ServerInfo {
     id: Snowflake,
     servers: string,
     users: string,
